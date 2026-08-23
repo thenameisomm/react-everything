1434d9061ebd330a7c50a0822995aef6def6b139
Om Badgujar <ombadgujar10@gmail.com>
2026-08-23 20:09:13 +0530

    New file structure created

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Counter from './day-01/Counter.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <Counter/>
 )
